a91f01a47d9569c6518bbdd495ef8fad7de134a7
nook-nattaphat-kaewpoo <82158813+nook-nattaphat-kaewpoo@users.noreply.github.com>
2021-04-08 22:22:26 +0900

    quick update

---
+++ apiurl.js
@@ -0,0 +1,4 @@
+//テスト環境下でのAjaxURL
+const is_test_environment = true;
+//※アップロードするときはファイル名をapiurl.jsに変更すること
+const ajax_url = "https://osjdc16uj8.execute-api.ap-northeast-1.amazonaws.com/dev/GazerFrontend_TEST";
