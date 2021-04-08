0454aecfb8dc845f636e601d80ebb47a871444cf
nook-nattaphat-kaewpoo <82158813+nook-nattaphat-kaewpoo@users.noreply.github.com>
2021-04-08 22:26:28 +0900

    Delete apiurl.js

--- apiurl.js
+++
@@ -1,4 +0,0 @@
-//テスト環境下でのAjaxURL
-const is_test_environment = true;
-//※アップロードするときはファイル名をapiurl.jsに変更すること
-const ajax_url = "https://osjdc16uj8.execute-api.ap-northeast-1.amazonaws.com/dev/GazerFrontend_TEST";
